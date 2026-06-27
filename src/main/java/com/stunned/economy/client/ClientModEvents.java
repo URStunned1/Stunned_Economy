2497d5b5771599b7e7f2ea96c10bc3871c409257
URStunned1 <Aaron.moulton88@gmail.com>
2026-06-26 22:17:18 -0500

    refactored

--- src/main/java/com/stunned/economy/client/ClientModEvents.java
+++ src/main/java/com/stunned/economy/client/ClientModEvents.java
@@ -1,12 +1,13 @@
 package com.stunned.economy.client;
 
 import com.stunned.economy.StunnedEconomy;
-import com.stunned.economy.client.screen.BankApplication;
 import com.stunned.economy.registry.ModMenuTypes;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.RegisterMenuScreensEvent;
+import com.stunned.economy.client.screen.AtmScreen;
+
 
 @EventBusSubscriber(
         modid = StunnedEconomy.MODID,
@@ -17,6 +18,6 @@ public class ClientModEvents {
 
     @SubscribeEvent
     public static void registerScreens(RegisterMenuScreensEvent event) {
-        event.register(ModMenuTypes.ATM_MENU.get(), BankApplication::new);
+        event.register(ModMenuTypes.ATM_MENU.get(), AtmScreen::new);
     }
 }
