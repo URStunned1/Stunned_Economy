8309b8a49deb3321c7493854db04c2c6954361d8
URStunned1 <Aaron.moulton88@gmail.com>
2026-07-05 12:48:04 -0500

    Clean terminal complete

--- src/main/java/com/stunned/economy/client/gui/IndustrialScreen.java
+++ src/main/java/com/stunned/economy/client/gui/IndustrialScreen.java
@@ -1,94 +1,67 @@
 package com.stunned.economy.client.gui;
 
+import com.stunned.economy.terminal.TerminalManager;
 import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.components.Renderable;
+import net.minecraft.client.gui.components.events.GuiEventListener;
+import net.minecraft.client.gui.narration.NarratableEntry;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.inventory.AbstractContainerMenu;
-import com.stunned.economy.terminal.TerminalManager;
 
 public abstract class IndustrialScreen<T extends AbstractContainerMenu>
         extends AbstractContainerScreen<T> {
 
-    public int getTerminalWidth() {
-        return this.imageWidth;
-    }
+    protected final TerminalManager terminalManager = new TerminalManager();
 
     public IndustrialScreen(T menu, Inventory inventory, Component title) {
         super(menu, inventory, title);
 
-        this.imageWidth = 240;
+        this.imageWidth = 350;
         this.imageHeight = 170;
 
         this.inventoryLabelX = -1000;
         this.inventoryLabelY = -1000;
     }
-    public <W extends net.minecraft.client.gui.components.Renderable &
-            net.minecraft.client.gui.components.events.GuiEventListener &
-            net.minecraft.client.gui.narration.NarratableEntry>
-    W addTerminalWidget(W widget) {
+
+    public int getTerminalWidth() {
+        return this.imageWidth;
+    }
+
+    public int getTerminalHeight() {
+        return this.imageHeight;
+    }
+
+    public <W extends Renderable & GuiEventListener & NarratableEntry> W addTerminalWidget(W widget) {
         return addRenderableWidget(widget);
     }
 
-    protected final TerminalManager terminalManager = new TerminalManager();
-
-    @Override
-    protected void renderBg(GuiGraphics guiGraphics, float partialTick, int mouseX, int mouseY) {
-
-        guiGraphics.fill(leftPos, topPos,
-                leftPos + imageWidth,
-                topPos + imageHeight,
-                0xFF1A1A1A);
-
-        guiGraphics.fill(leftPos + 4, topPos + 4,
-                leftPos + imageWidth - 4,
-                topPos + 22,
-                0xFF2E3A46);
-
-        guiGraphics.fill(leftPos + 4, topPos + 26,
-                leftPos + imageWidth - 4,
-                topPos + imageHeight - 20,
-                0xFF101820);
-
-        guiGraphics.fill(leftPos + 4,
-                topPos + imageHeight - 16,
-                leftPos + imageWidth - 4,
-                topPos + imageHeight - 4,
-                0xFF2A2A2A);
-
-        guiGraphics.drawString(
-                font,
-                "STUNNED INDUSTRIES TERMINAL",
-                leftPos + 10,
-                topPos + 10,
-                0xFFFFFF
-        );
-
-        guiGraphics.drawString(
-                font,
-                "ONLINE",
-                leftPos + 10,
-                topPos + imageHeight - 13,
-                0x55FF55
-        );
-        terminalManager.getActiveApplication().render(
-                guiGraphics,
-                leftPos,
-                topPos,
-                mouseX,
-                mouseY,
-                partialTick
-        );
-    }
-
-    @Override
-    public boolean isPauseScreen() {
-        return false;
-    }
     @Override
     protected void init() {
         super.init();
         terminalManager.getActiveApplication().init(this);
     }
 
+    @Override
+    protected void renderBg(GuiGraphics guiGraphics, float partialTick, int mouseX, int mouseY) {
+        guiGraphics.fill(leftPos, topPos, leftPos + imageWidth, topPos + imageHeight, 0xFF1A1A1A);
+
+        guiGraphics.fill(leftPos + 4, topPos + 4, leftPos + imageWidth - 4, topPos + 22, 0xFF2E3A46);
+
+        guiGraphics.fill(leftPos + 4, topPos + 26, leftPos + imageWidth - 4, topPos + imageHeight - 20, 0xFF101820);
+
+        guiGraphics.fill(leftPos + 4, topPos + imageHeight - 16, leftPos + imageWidth - 4, topPos + imageHeight - 4, 0xFF2A2A2A);
+
+        guiGraphics.drawString(font, "STUNNED INDUSTRIES TERMINAL", leftPos + 10, topPos + 10, 0xFFFFFF);
+
+        guiGraphics.drawString(font, "ONLINE", leftPos + 10, topPos + imageHeight - 13, 0x55FF55);
+
+        terminalManager.getActiveApplication().render(guiGraphics, leftPos, topPos, mouseX, mouseY, partialTick);
+    }
+
+    @Override
+    public boolean isPauseScreen() {
+        return false;
+    }
 }
--- src/main/java/com/stunned/economy/menu/AtmMenu.java
+++ src/main/java/com/stunned/economy/menu/AtmMenu.java
@@ -1,35 +1,102 @@
 package com.stunned.economy.menu;
 
+import com.stunned.economy.bank.BankAccountManager;
+import com.stunned.economy.bank.BankCommands;
+import com.stunned.economy.registry.ModItems;
 import com.stunned.economy.registry.ModMenuTypes;
 import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.inventory.DataSlot;
 import net.minecraft.world.item.ItemStack;
-import com.stunned.economy.bank.BankCommands;
-import net.minecraft.server.level.ServerPlayer;
 
 public class AtmMenu extends AbstractContainerMenu {
 
-    public AtmMenu(int containerId, Inventory inventory, FriendlyByteBuf extraData) {
-        this(containerId, inventory);
-    }
     public static final int BUTTON_DEPOSIT = 0;
     public static final int BUTTON_WITHDRAW_100 = 1;
     public static final int BUTTON_BALANCE = 2;
 
+    private int balance = 0;
+    private int cashOnHand = 0;
+
+    public AtmMenu(int containerId, Inventory inventory, FriendlyByteBuf extraData) {
+        this(containerId, inventory);
+    }
+
     public AtmMenu(int containerId, Inventory inventory) {
         super(ModMenuTypes.ATM_MENU.get(), containerId);
+
+        refreshValues(inventory.player);
+
+        addDataSlot(new DataSlot() {
+            @Override
+            public int get() {
+                return balance;
+            }
+
+            @Override
+            public void set(int value) {
+                balance = value;
+            }
+        });
+
+        addDataSlot(new DataSlot() {
+            @Override
+            public int get() {
+                return cashOnHand;
+            }
+
+            @Override
+            public void set(int value) {
+                cashOnHand = value;
+            }
+        });
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public int getCashOnHand() {
+        return cashOnHand;
+    }
+
+    private void refreshValues(Player player) {
+        if (player instanceof ServerPlayer serverPlayer) {
+            balance = Math.toIntExact(BankAccountManager.getBalance(serverPlayer));
+        }
+
+        cashOnHand = countCash(player.getInventory());
+    }
+
+    private int countCash(Inventory inventory) {
+        int total = 0;
+
+        for (int i = 0; i < inventory.getContainerSize(); i++) {
+            ItemStack stack = inventory.getItem(i);
+
+            if (stack.is(ModItems.CREDIT_1.get())) total += stack.getCount();
+            else if (stack.is(ModItems.CREDIT_5.get())) total += stack.getCount() * 5;
+            else if (stack.is(ModItems.CREDIT_10.get())) total += stack.getCount() * 10;
+            else if (stack.is(ModItems.CREDIT_50.get())) total += stack.getCount() * 50;
+            else if (stack.is(ModItems.CREDIT_100.get())) total += stack.getCount() * 100;
+        }
+
+        return total;
     }
 
     @Override
     public boolean stillValid(Player player) {
         return true;
     }
+
     @Override
     public ItemStack quickMoveStack(Player player, int index) {
         return ItemStack.EMPTY;
     }
+
     @Override
     public boolean clickMenuButton(Player player, int id) {
         if (!(player instanceof ServerPlayer serverPlayer)) {
@@ -37,22 +104,16 @@ public class AtmMenu extends AbstractContainerMenu {
         }
 
         switch (id) {
-            case BUTTON_DEPOSIT -> {
-                BankCommands.deposit(serverPlayer);
-                return true;
-            }
-
-            case BUTTON_WITHDRAW_100 -> {
-                BankCommands.withdraw(serverPlayer, 100);
-                return true;
-            }
-
-            case BUTTON_BALANCE -> {
-                BankCommands.balance(serverPlayer);
-                return true;
+            case BUTTON_DEPOSIT -> BankCommands.deposit(serverPlayer);
+            case BUTTON_WITHDRAW_100 -> BankCommands.withdraw(serverPlayer, 100);
+            case BUTTON_BALANCE -> BankCommands.balance(serverPlayer);
+            default -> {
+                return false;
             }
         }
 
-        return false;
+        refreshValues(serverPlayer);
+        broadcastChanges();
+        return true;
     }
 }
